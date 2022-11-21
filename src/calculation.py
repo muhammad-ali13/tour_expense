804232618af401cfa13b6ecfb96dac84b2c21cd7
sezan <sezan92>
2022-11-22 01:18:14 +0900

    updated calculation

--- src/calculation.py
+++ src/calculation.py
@@ -1,4 +1,4 @@
-
+import pandas as pd
 class Member:
     def __init__(self, name: str) -> None:
         self.name = name
@@ -6,25 +6,28 @@ class Member:
         
 
 if __name__ == "__main__":
-    member1 = Member("mim")
-    member2 = Member("sezan")
-    member3 = Member("ali")
-    member1.given[member2] = 100.0 # member1 to member2
-    member2.given[member1] = 200.0 # member2 to member1
-    member2.given[member3] = 300.0 # member2 to member3
-    member3.given[member1] = 100.0
-    member1.given[member3] = 200.0
-    members = [member1, member2, member3]
-    for member in members:
-        name = member.name
+    csv_file = "/src/tour_cost.csv"
+    member_col_idx_start = 4
+    member_col_idx_end = 12
+    paid_name_column_name = 'who'
+    df = pd.read_csv(csv_file)
+    columns = df.columns
+
+    members = {member_name:Member(member_name) for member_name in columns[member_col_idx_start:member_col_idx_end + 1]}
+    for member_name in columns[member_col_idx_start:member_col_idx_end + 1]:
+        for received_member in members.values():
+                    members[member_name].given[received_member] = 0
+
+    for index, row in df.iterrows():
+        indiv_amount = row['how much'] / row['total number']
+        for received_member in members.values():
+            flag = 1 if row['ALL'] else row[received_member.name]
+            members[row['who']].given[received_member] += indiv_amount * flag
+    for name in list(members.keys()):
+        member = members[name]
         for k_member in list(member.given.keys()):
-            if member in k_member.given:
-                due = member.given[k_member] - k_member.given[member]
-            else:
-                due = member.given[k_member]
-            if due > 0:
+
+            due = member.given[k_member] - k_member.given[member]
+            
+            if abs(due) > 0:
                 print(f'{name} will get {due} from {k_member.name}')
-            elif due < 0:
-                print(f'{name} will return {abs(due)} to {k_member.name}')
-            else:
-                pass
